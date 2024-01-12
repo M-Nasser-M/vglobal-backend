9f97bbc947d5b1356aae04ed46272db525236699
mohamed nasser <m.nasser.15910@gmail.com>
2024-01-12 17:37:36 +0200

    fix:change url to use Strapi_url instead as env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  URL: env("URL", "http://localhost:1337"),
+  URL: env("STRAPI_URL", "http://localhost:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
